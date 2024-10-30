bc33842df8f46681451b673f94e0b9e0d864f2b0
Yan <yantmartins@live.com>
2024-10-30 16:54:38 -0400

    first commit

---
+++ exemploVariavel.php
@@ -0,0 +1,9 @@
+<?php
+    $nome = "Yan";
+    $sobrenome = "Torres";
+    $idade = "25";
+    $profissao = "Dev";
+
+    echo "Olá $profissao $nome $sobrenome. <br>";
+    echo "Você possui atualmente $idade anos.";
+?>
